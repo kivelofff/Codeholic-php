18f3a815bd6a40e3fa60dd3df4136f268b4ebdbd
stupid_laborant <ivan.elantyev@yandex.ru>
2021-12-03 01:38:12 +0100

    oop done

--- 14_oop/index.php
+++ 14_oop/index.php
@@ -1,7 +1,27 @@
 <?php
 
 // What is class and instance
+class Person {
+    public string $name;
+    public int $age;
+    public ?int $salary; //null can be assigned
 
+    public function __construct($name, $age, $salary)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
+}
+
+$p = new Person();
+$p->name = 'Doe';
+$p->age = 14;
+$p->salary = 10000;
+
+echo '<pre>';
+var_dump($p);
+echo '</pre>';
 // Create Person class in Person.php
 
 // Create instance of Person
